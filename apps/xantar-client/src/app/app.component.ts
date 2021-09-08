19d491bf5001e8eb764c7f7588911c9d346910e5
Fabio <fabioiglesias@hotmail.es>
2021-09-08 19:25:50 +0200

    initial commit

---
+++ apps/xantar-client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xantar-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'xantar-client';
+}
