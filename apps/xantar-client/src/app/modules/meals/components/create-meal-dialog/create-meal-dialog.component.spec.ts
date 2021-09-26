72dd71fa1cab1702a79aacc2a0ede22eb48f61e9
Fabio <fabioiglesias@hotmail.es>
2021-09-26 21:55:11 +0200

    [X-1] pr suggestions

--- apps/xantar-client/src/app/modules/meals/components/create-meal-dialog/create-meal-dialog.component.spec.ts
+++ apps/xantar-client/src/app/modules/meals/components/create-meal-dialog/create-meal-dialog.component.spec.ts
@@ -42,7 +42,7 @@ class DialogLaunchComponent {
   }
 }
 
-describe('CreateMealDialogComponent', () => {
+describe('CreateMealDialogComponent from launcher', () => {
   let component: DialogLaunchComponent;
   let fixture: ComponentFixture<DialogLaunchComponent>;
   let rootLoader: HarnessLoader;
@@ -81,7 +81,7 @@ describe('CreateMealDialogComponent', () => {
 
   afterEach(async () => {
     const dialogs = await rootLoader.getAllHarnesses(MatDialogHarness);
-    await Promise.all(dialogs.map(async (d) => await d.close()));
+    await Promise.all(dialogs.map(async (d) => d.close()));
   });
 
   it('should create', () => {
@@ -100,156 +100,10 @@ describe('CreateMealDialogComponent', () => {
   describe('Mat inputs', () => {
     let dialogRef: MatDialogRef<CreateMealDialogComponent>;
     let dialogHarness: MatDialogHarness;
-    let createButtonHarness: MatButtonHarness;
 
     beforeEach(async () => {
       dialogRef = component.openCreateMealDialog();
       dialogHarness = await rootLoader.getHarness(MatDialogHarness);
-      createButtonHarness = await dialogHarness.getHarness(
-        MatButtonHarness.with({ selector: '.create-button' })
-      );
-    });
-
-    it('should show name input', (done) => {
-      from(
-        dialogHarness.getHarness(
-          MatInputHarness.with({ selector: '.name-input' })
-        )
-      ).subscribe((nameInputHarness: MatInputHarness) => {
-        expect(nameInputHarness).toBeTruthy();
-        from(nameInputHarness.setValue('test name')).subscribe(() => {
-          dialogRef.afterClosed().subscribe((mealSumup) => {
-            expect(mealSumup).toBeTruthy();
-            expect(mealSumup.name).toBe('test name');
-            done();
-          });
-          createButtonHarness.click();
-        });
-      });
-    });
-
-    it('should show description input', (done) => {
-      from(
-        dialogHarness.getHarness(
-          MatInputHarness.with({ selector: '.description-input' })
-        )
-      ).subscribe((descriptionInputHarness: MatInputHarness) => {
-        expect(descriptionInputHarness).toBeTruthy();
-        from(descriptionInputHarness.setValue('test description')).subscribe(
-          () => {
-            dialogRef.afterClosed().subscribe((mealSumup) => {
-              expect(mealSumup).toBeTruthy();
-              expect(mealSumup.description).toBe('test description');
-              done();
-            });
-            createButtonHarness.click();
-          }
-        );
-      });
-    });
-
-    it('should show select input', (done) => {
-      from(
-        dialogHarness.getHarness(
-          MatSelectHarness.with({ selector: '.slots-input' })
-        )
-      ).subscribe((selectInputHarness: MatSelectHarness) => {
-        expect(selectInputHarness).toBeTruthy();
-        from(selectInputHarness.clickOptions()).subscribe(() => {
-          dialogRef.afterClosed().subscribe((mealSumup) => {
-            expect(mealSumup).toBeTruthy();
-            expect(JSON.stringify(mealSumup.slots)).toBe(
-              JSON.stringify([
-                { id: 1, name: 'BREAKFAST' },
-                { id: 2, name: 'SNACK1' },
-                { id: 3, name: 'LUNCH' },
-                { id: 4, name: 'SNACK2' },
-                { id: 5, name: 'DINNER' }
-              ])
-            );
-            done();
-          });
-          createButtonHarness.click();
-        });
-      });
-    });
-
-    describe('chips list', () => {
-      it('should show attributes input', (done) => {
-        from(dialogHarness.getHarness(MatChipInputHarness)).subscribe(
-          (attributesInputFieldHarness: MatChipInputHarness) => {
-            from(
-              attributesInputFieldHarness.setValue('test-attribute')
-            ).subscribe(() => {
-              from(
-                attributesInputFieldHarness.sendSeparatorKey(TestKey.ENTER)
-              ).subscribe(() => {
-                from(
-                  dialogHarness.getHarness(
-                    MatChipListHarness.with({ selector: '.attributes-input' })
-                  )
-                ).subscribe((attributesInputHarness: MatChipListHarness) => {
-                  expect(attributesInputHarness).toBeTruthy();
-                  from(attributesInputHarness.getChips()).subscribe(
-                    (chips: MatChipHarness[]) => {
-                      expect(chips?.length).toBe(1);
-                      from(chips[0].getText()).subscribe((chipText) => {
-                        expect(chipText).toBe('test-attribute');
-                        dialogRef.afterClosed().subscribe((mealSumup) => {
-                          expect(mealSumup).toBeTruthy();
-                          expect(JSON.stringify(mealSumup.attributes)).toBe(
-                            JSON.stringify(['test-attribute'])
-                          );
-                          done();
-                        });
-                        createButtonHarness.click();
-                      });
-                    }
-                  );
-                });
-              });
-            });
-          }
-        );
-      });
-
-      it('should delete attributes input', (done) => {
-        from(dialogHarness.getHarness(MatChipInputHarness)).subscribe(
-          (attributesInputFieldHarness: MatChipInputHarness) => {
-            from(
-              attributesInputFieldHarness.setValue('test-attribute')
-            ).subscribe(() => {
-              from(
-                attributesInputFieldHarness.sendSeparatorKey(TestKey.ENTER)
-              ).subscribe(() => {
-                from(
-                  dialogHarness.getHarness(
-                    MatChipListHarness.with({ selector: '.attributes-input' })
-                  )
-                ).subscribe((attributesInputHarness: MatChipListHarness) => {
-                  expect(attributesInputHarness).toBeTruthy();
-                  from(attributesInputHarness.getChips()).subscribe(
-                    (chips: MatChipHarness[]) => {
-                      expect(chips?.length).toBe(1);
-                      from(chips[0].getText()).subscribe((chipText) => {
-                        expect(chipText).toBe('test-attribute');
-
-                        from(chips[0].remove()).subscribe(() => {
-                          dialogRef.afterClosed().subscribe((mealSumup) => {
-                            expect(mealSumup.attributes.length).toBe(0);
-                            done();
-                          });
-                          createButtonHarness.click();
-                        });
-                      });
-                    }
-                  );
-                });
-              });
-            });
-          }
-        );
-      });
     });
 
     it('should cancel the creation', (done) => {
@@ -269,7 +123,7 @@ describe('CreateMealDialogComponent', () => {
   });
 });
 
-describe('CreateMealDialogComponent Unit Tests', () => {
+describe('CreateMealDialogComponent', () => {
   let file: File;
   let component: CreateMealDialogComponent;
   let fixture: ComponentFixture<CreateMealDialogComponent>;
@@ -307,135 +161,237 @@ describe('CreateMealDialogComponent Unit Tests', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+  describe('Unit Tests', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    describe('onImageSelected', () => {
+      it('should return without loading if file is null', (done) => {
+        expect(component.mealForm.get('imageThumb')?.value).toBeNull();
 
-  describe('onImageSelected', () => {
-    it('should return without loading if file is null', (done) => {
-      expect(component.mealForm.get('imageThumb')?.value).toBeNull();
+        component
+          .onImageSelected({ files: [] as unknown as FileList })
+          .subscribe({
+            complete: () => {
+              expect(component.mealForm.get('imageThumb')?.value).toBeNull();
+              done();
+            }
+          });
+      });
 
-      component
-        .onImageSelected({ files: [] as unknown as FileList })
-        .subscribe({
-          complete: () => {
-            expect(component.mealForm.get('imageThumb')?.value).toBeNull();
+      it('should load a file', (done) => {
+        expect(component.mealForm.get('imageThumb')?.value).toBeNull();
+
+        component
+          .onImageSelected({ files: [file] as unknown as FileList })
+          .subscribe(() => {
+            expect(component.mealForm.get('imageThumb')?.value).toBe(
+              'data:application/octet-stream;base64,dGVzdA=='
+            );
             done();
-          }
-        });
+          });
+      });
     });
 
-    it('should load a file', (done) => {
-      expect(component.mealForm.get('imageThumb')?.value).toBeNull();
+    describe('slotSelectionChange', () => {
+      it('should set text value to ""', () => {
+        component.slotsTextValue = 'test';
+        expect(component.slotsTextValue).toBe('test');
 
-      component
-        .onImageSelected({ files: [file] as unknown as FileList })
-        .subscribe(() => {
-          expect(component.mealForm.get('imageThumb')?.value).toBe(
-            'data:application/octet-stream;base64,dGVzdA=='
-          );
-          done();
-        });
+        component.slotSelectionChange({ value: [] } as MatSelectChange);
+        expect(component.slotsTextValue).toBe('');
+      });
+
+      it('should translate and concatenate slots', () => {
+        component.slotSelectionChange({
+          value: ['slot1', 'slot2', 'slot3']
+        } as MatSelectChange);
+        expect(component.slotsTextValue).toBe(
+          'meals.slots.undefined, meals.slots.undefined, meals.slots.undefined'
+        );
+      });
+    });
+
+    describe('removeAttribute', () => {
+      it('should remove an attribute', () => {
+        component.attributes = ['attr1', 'attr2', 'attr3'];
+
+        component.removeAttribute('attr2');
+        expect(component.attributes).toHaveLength(2);
+        expect(component.attributes).not.toContain('attr2');
+      });
+
+      it('should not remove an attribute if not found', () => {
+        component.attributes = ['attr1', 'attr2', 'attr3'];
+
+        component.removeAttribute('attrX');
+        expect(component.attributes).toHaveLength(3);
+      });
+    });
+
+    describe('addAttr', () => {
+      it('should add an attribute', () => {
+        expect(component.mealForm.get('attributes')?.value).toBeNull();
+        expect(component.attributes).toHaveLength(0);
+
+        component.addAttr({ value: 'attribute' } as MatChipInputEvent);
+
+        expect(component.attributes).toHaveLength(1);
+        expect(component.attributes).toContain('attribute');
+        expect(component.mealForm.get('attributes')?.value).toContain(
+          'attribute'
+        );
+      });
+
+      it('should not add an attribute if it is falsy', () => {
+        component.addAttr({ value: '' } as MatChipInputEvent);
+
+        expect(component.attributes).toHaveLength(0);
+        expect(component.mealForm.get('attributes')?.value).toBeNull();
+      });
+
+      it('should not add a duplicate attribute', () => {
+        component.attributes = ['attr1'];
+        component.addAttr({ value: 'attr1' } as MatChipInputEvent);
+        component.mealForm.get('attributes')?.patchValue(component.attributes);
+
+        expect(component.attributes).toHaveLength(1);
+        expect(component.attributes).toContain('attr1');
+        expect(component.mealForm.get('attributes')?.value).toContain('attr1');
+      });
+
+      it('should call MatChipInput clear function', () => {
+        const mockChipEvent = {
+          value: 'attr1',
+          chipInput: { clear: jest.fn() } as unknown
+        } as MatChipInputEvent;
+        component.addAttr(mockChipEvent);
+
+        expect(mockChipEvent.chipInput?.clear).toHaveBeenCalled();
+      });
+    });
+
+    describe('submit', () => {
+      it('should extract data from FormGroup and return it through the dialog ref', () => {
+        const mockMealSumup: IMealSumup = {
+          name: 'test name',
+          description: 'test description',
+          imageThumb: 'test image',
+          slots: [slotsList.breakfast],
+          attributes: ['test attribute']
+        } as unknown as IMealSumup;
+
+        const closeSpy = jest.spyOn(component['dialogRef'], 'close');
+
+        expect(closeSpy).not.toHaveBeenCalled();
+
+        component.mealForm.patchValue(mockMealSumup);
+        component.submit();
+
+        expect(closeSpy).toHaveBeenCalledWith(mockMealSumup);
+      });
     });
   });
 
-  describe('slotSelectionChange', () => {
-    it('should set text value to ""', () => {
-      component.slotsTextValue = 'test';
-      expect(component.slotsTextValue).toBe('test');
+  describe('Integration Tests', () => {
+    let rootLoader: HarnessLoader;
 
-      component.slotSelectionChange({ value: [] } as MatSelectChange);
-      expect(component.slotsTextValue).toBe('');
+    beforeEach(() => {
+      rootLoader = TestbedHarnessEnvironment.documentRootLoader(fixture);
     });
 
-    it('should translate and concatenate slots', () => {
-      component.slotSelectionChange({
-        value: ['slot1', 'slot2', 'slot3']
-      } as MatSelectChange);
-      expect(component.slotsTextValue).toBe(
-        'meals.slots.undefined, meals.slots.undefined, meals.slots.undefined'
+    afterEach(async () => {
+      const dialogs = await rootLoader.getAllHarnesses(MatDialogHarness);
+      await Promise.all(dialogs.map(async (d) => d.close()));
+    });
+
+    it('should show name input', async () => {
+      const nameInputHarness = await rootLoader.getHarness(
+        MatInputHarness.with({ selector: '.name-input' })
+      );
+      await nameInputHarness.setValue('test name');
+
+      expect(component.mealForm.get('name')?.value).toBe('test name');
+    });
+
+    it('should show description input', async () => {
+      const descriptionInputHarness = await rootLoader.getHarness(
+        MatInputHarness.with({ selector: '.description-input' })
+      );
+      await descriptionInputHarness.setValue('test description');
+
+      expect(component.mealForm.get('description')?.value).toBe(
+        'test description'
       );
     });
-  });
 
-  describe('removeAttribute', () => {
-    it('should remove an attribute', () => {
-      component.attributes = ['attr1', 'attr2', 'attr3'];
+    it('should show select input', async () => {
+      const slotSelectHarness = await rootLoader.getHarness(
+        MatSelectHarness.with({ selector: '.slots-input' })
+      );
+      await slotSelectHarness.clickOptions();
 
-      component.removeAttribute('attr2');
-      expect(component.attributes).toHaveLength(2);
-      expect(component.attributes).not.toContain('attr2');
-    });
-
-    it('should not remove an attribute if not found', () => {
-      component.attributes = ['attr1', 'attr2', 'attr3'];
-
-      component.removeAttribute('attrX');
-      expect(component.attributes).toHaveLength(3);
-    });
-  });
-
-  describe('addAttr', () => {
-    it('should add an attribute', () => {
-      expect(component.mealForm.get('attributes')?.value).toBeNull();
-      expect(component.attributes).toHaveLength(0);
-
-      component.addAttr({ value: 'attribute' } as MatChipInputEvent);
-
-      expect(component.attributes).toHaveLength(1);
-      expect(component.attributes).toContain('attribute');
-      expect(component.mealForm.get('attributes')?.value).toContain(
-        'attribute'
+      expect(JSON.stringify(component.mealForm.get('slots')?.value)).toBe(
+        JSON.stringify([
+          { id: 1, name: 'BREAKFAST' },
+          { id: 2, name: 'SNACK1' },
+          { id: 3, name: 'LUNCH' },
+          { id: 4, name: 'SNACK2' },
+          { id: 5, name: 'DINNER' }
+        ])
       );
     });
 
-    it('should not add an attribute if it is falsy', () => {
-      component.addAttr({ value: '' } as MatChipInputEvent);
+    describe('chips list', () => {
+      it('should show attributes input', async () => {
+        const chipInputHarness = await rootLoader.getHarness(
+          MatChipInputHarness
+        );
 
-      expect(component.attributes).toHaveLength(0);
-      expect(component.mealForm.get('attributes')?.value).toBeNull();
-    });
+        await chipInputHarness.setValue('test-attribute');
+        await chipInputHarness.sendSeparatorKey(TestKey.ENTER);
 
-    it('should not add a duplicate attribute', () => {
-      component.attributes = ['attr1'];
-      component.addAttr({ value: 'attr1' } as MatChipInputEvent);
-      component.mealForm.get('attributes')?.patchValue(component.attributes);
+        const chipListHarness = await rootLoader.getHarness(
+          MatChipListHarness.with({ selector: '.attributes-input' })
+        );
+        expect(chipListHarness).toBeTruthy();
 
-      expect(component.attributes).toHaveLength(1);
-      expect(component.attributes).toContain('attr1');
-      expect(component.mealForm.get('attributes')?.value).toContain('attr1');
-    });
+        const chips: MatChipHarness[] = await chipListHarness.getChips();
 
-    it('should call MatChipInput clear function', () => {
-      const mockChipEvent = {
-        value: 'attr1',
-        chipInput: { clear: jest.fn() } as unknown
-      } as MatChipInputEvent;
-      component.addAttr(mockChipEvent);
+        expect(chips?.length).toBe(1);
 
-      expect(mockChipEvent.chipInput?.clear).toHaveBeenCalled();
-    });
-  });
+        const chipText = await chips[0].getText();
+        expect(chipText).toBe('test-attribute');
 
-  describe('submit', () => {
-    it('should extract data from FormGroup and return it through the dialog ref', () => {
-      const mockMealSumup: IMealSumup = {
-        name: 'test name',
-        description: 'test description',
-        imageThumb: 'test image',
-        slots: [slotsList.breakfast],
-        attributes: ['test attribute']
-      } as unknown as IMealSumup;
+        const attributes: string[] =
+          component.mealForm.get('attributes')?.value;
+        expect(attributes.length).toBe(1);
+        expect(attributes[0]).toBe('test-attribute');
+      });
 
-      const closeSpy = jest.spyOn(component['dialogRef'], 'close');
+      it('should delete attributes input', async () => {
+        const chipInputHarness = await rootLoader.getHarness(
+          MatChipInputHarness
+        );
 
-      expect(closeSpy).not.toHaveBeenCalled();
+        await chipInputHarness.setValue('test-attribute');
+        await chipInputHarness.sendSeparatorKey(TestKey.ENTER);
 
-      component.mealForm.patchValue(mockMealSumup);
-      component.submit();
+        const chipListHarness = await rootLoader.getHarness(
+          MatChipListHarness.with({ selector: '.attributes-input' })
+        );
 
-      expect(closeSpy).toHaveBeenCalledWith(mockMealSumup);
+        const chips: MatChipHarness[] = await chipListHarness.getChips();
+
+        expect(chips?.length).toBe(1);
+
+        await chips[0].remove();
+
+        const attributes: string[] =
+          component.mealForm.get('attributes')?.value;
+        expect(attributes.length).toBe(0);
+      });
     });
   });
 });
--- apps/xantar-client/src/app/modules/shared/components/toolbar/toolbar.component.spec.ts
+++ apps/xantar-client/src/app/modules/shared/components/toolbar/toolbar.component.spec.ts
@@ -2,9 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MATERIAL_SANITY_CHECKS } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { By } from '@angular/platform-browser';
+import { IIconButtonSettings } from '@xantar/domain/models';
+import { Subject } from 'rxjs';
+import { getTranslocoModule } from '../../../../transloco-testing.module';
+import { ToolbarService } from '../../services/toolbar/toolbar.service';
 import { ToolbarComponent } from './toolbar.component';
 
-const materialModules = [MatToolbarModule, MatIconModule];
+const materialModules = [MatToolbarModule, MatIconModule, MatTooltipModule];
 describe('ToolbarComponent', () => {
   let component: ToolbarComponent;
   let fixture: ComponentFixture<ToolbarComponent>;
@@ -12,7 +18,7 @@ describe('ToolbarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ToolbarComponent],
-      imports: materialModules,
+      imports: [...materialModules, getTranslocoModule()],
       providers: [{ provide: MATERIAL_SANITY_CHECKS, useValue: false }]
     }).compileComponents();
   });
@@ -20,10 +26,44 @@ describe('ToolbarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ToolbarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should show title', () => {
+    const toolbarService = TestBed.inject(ToolbarService);
+    toolbarService.title = 'Test title';
+
+    fixture.detectChanges();
+
+    const title = fixture.debugElement.query(By.css('.toolbar-title'));
+
+    expect(title).toBeTruthy();
+    expect(title.nativeElement.textContent).toBe('Test title');
+  });
+
+  it('should configure add button', (done) => {
+    const clickSubject = new Subject<boolean>();
+    const addSettings: IIconButtonSettings = {
+      tooltip: 'test',
+      clickSubject
+    };
+
+    const toolbarService = TestBed.inject(ToolbarService);
+    toolbarService.addButtonSettings = addSettings;
+
+    fixture.detectChanges();
+
+    clickSubject.subscribe((value) => {
+      expect(value).toBe(true);
+      done();
+    });
+
+    const addButton = fixture.debugElement.query(By.css('.create-meal-button'));
+    expect(addButton).toBeTruthy();
+    addButton.nativeElement.click();
   });
 });
