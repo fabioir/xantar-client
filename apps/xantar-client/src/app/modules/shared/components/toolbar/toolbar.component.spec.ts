4f52105ec97eb8d8cdf9e1301b7b4c970817183f
Fabio <fabioiglesias@hotmail.es>
2021-10-02 21:18:29 +0200

    [X-4] routing to schedule view

--- apps/xantar-client/src/app/modules/shared/components/toolbar/toolbar.component.spec.ts
+++ apps/xantar-client/src/app/modules/shared/components/toolbar/toolbar.component.spec.ts
@@ -1,9 +1,13 @@
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
 import { MATERIAL_SANITY_CHECKS } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 import { IIconButtonSettings } from '@xantar/domain/models';
 import { Subject } from 'rxjs';
 import { getTranslocoModule } from '../../../../transloco-testing.module';
@@ -11,14 +15,23 @@ import { ToolbarService } from '../../services/toolbar/toolbar.service';
 import { ToolbarComponent } from './toolbar.component';
 
 const materialModules = [MatToolbarModule, MatIconModule, MatTooltipModule];
+
 describe('ToolbarComponent', () => {
   let component: ToolbarComponent;
   let fixture: ComponentFixture<ToolbarComponent>;
+  let rootLoader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ToolbarComponent],
-      imports: [...materialModules, getTranslocoModule()],
+      imports: [
+        ...materialModules,
+        getTranslocoModule(),
+        RouterTestingModule.withRoutes([
+          { path: 'meals', component: ToolbarComponent },
+          { path: 'schedule', component: ToolbarComponent }
+        ])
+      ],
       providers: [{ provide: MATERIAL_SANITY_CHECKS, useValue: false }]
     }).compileComponents();
   });
@@ -26,6 +39,7 @@ describe('ToolbarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ToolbarComponent);
     component = fixture.componentInstance;
+    rootLoader = TestbedHarnessEnvironment.documentRootLoader(fixture);
   });
 
   it('should create', () => {
@@ -45,7 +59,8 @@ describe('ToolbarComponent', () => {
     expect(title.nativeElement.textContent).toBe('Test title');
   });
 
-  it('should configure add button', (done) => {
+  it('should configure add button', async () => {
+    let clicked = false;
     const clickSubject = new Subject<boolean>();
     const addSettings: IIconButtonSettings = {
       tooltip: 'test',
@@ -59,11 +74,49 @@ describe('ToolbarComponent', () => {
 
     clickSubject.subscribe((value) => {
       expect(value).toBe(true);
-      done();
+      clicked = true;
     });
 
-    const addButton = fixture.debugElement.query(By.css('.create-meal-button'));
-    expect(addButton).toBeTruthy();
-    addButton.nativeElement.click();
+    const addMealButtonHarness = await rootLoader.getHarness(
+      MatButtonHarness.with({ selector: '.create-meal-button' })
+    );
+    expect(addMealButtonHarness).toBeTruthy();
+    await addMealButtonHarness.click();
+    expect(clicked).toBe(true);
+  });
+
+  describe('navigation', () => {
+    beforeEach(() => {
+      fixture.detectChanges();
+    });
+    it('should navigate to schedule, show go to meals list anchor and hide schedule button', async () => {
+      const toScheduleButtonHarness = await rootLoader.getHarness(
+        MatButtonHarness.with({ selector: '.schedule-anchor' })
+      );
+      expect(toScheduleButtonHarness).toBeTruthy();
+      await toScheduleButtonHarness.click();
+
+      const anchorButtons = await rootLoader.getAllHarnesses(
+        MatButtonHarness.with({ selector: 'a' })
+      );
+      expect(anchorButtons.length).toBe(1);
+      const anchorButtonText = await anchorButtons[0].getText();
+      expect(anchorButtonText).toBe('format_list_bulleted');
+    });
+
+    it('should navigate to meals list, show go to schedule anchor and hide meals list button', async () => {
+      const toScheduleButtonHarness = await rootLoader.getHarness(
+        MatButtonHarness.with({ selector: '.meals-list-anchor' })
+      );
+      expect(toScheduleButtonHarness).toBeTruthy();
+      await toScheduleButtonHarness.click();
+
+      const anchorButtons = await rootLoader.getAllHarnesses(
+        MatButtonHarness.with({ selector: 'a' })
+      );
+      expect(anchorButtons.length).toBe(1);
+      const anchorButtonText = await anchorButtons[0].getText();
+      expect(anchorButtonText).toBe('restaurant');
+    });
   });
 });
--- apps/xantar-client/src/app/modules/shared/shared.module.ts
+++ apps/xantar-client/src/app/modules/shared/shared.module.ts
@@ -4,11 +4,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
 import { TranslocoModule } from '@ngneat/transloco';
+import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
 import { ReloadContentComponent } from './components/reload-content/reload-content.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { B64ImgPipe } from './pipes/b64Img/b64Img.pipe';
-import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
 
 const materialModules = [
   MatButtonModule,
@@ -18,8 +19,13 @@ const materialModules = [
 ];
 
 @NgModule({
-  declarations: [B64ImgPipe, ReloadContentComponent, ToolbarComponent, ConfirmationDialogComponent],
-  imports: [...materialModules, TranslocoModule, CommonModule],
+  declarations: [
+    B64ImgPipe,
+    ReloadContentComponent,
+    ToolbarComponent,
+    ConfirmationDialogComponent
+  ],
+  imports: [...materialModules, TranslocoModule, CommonModule, RouterModule],
   exports: [B64ImgPipe, ReloadContentComponent, ToolbarComponent]
 })
 export class SharedModule {}
