e4775f6e1167462ec13c7a22ad978edfb0163010
Fabio <fabioiglesias@hotmail.es>
2021-10-02 21:38:56 +0200

    [X-4] routing to schedule view (#43)
    * [X-4] routing to schedule view
    
    * [X-4] remove duplications in tests

---
+++ apps/xantar-client/src/app/modules/schedule/components/schedule/schedule.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xantar-schedule',
+  templateUrl: './schedule.component.html',
+  styleUrls: ['./schedule.component.scss']
+})
+export class ScheduleComponent {}
