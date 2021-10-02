beced2f376ccde0ffcf19c8a44e7d99bdbfb459a
Fabio <fabioiglesias@hotmail.es>
2021-10-03 00:05:26 +0200

    [X-4] set toolbar title

--- apps/xantar-client/src/app/modules/schedule/components/schedule/schedule.component.spec.ts
+++ apps/xantar-client/src/app/modules/schedule/components/schedule/schedule.component.spec.ts
@@ -1,5 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MATERIAL_SANITY_CHECKS } from '@angular/material/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { getTranslocoModule } from '../../../../transloco-testing.module';
+import { ToolbarService } from '../../../shared/services/toolbar/toolbar.service';
+import { DayComponent } from '../day/day.component';
+import { TileComponent } from '../tile/tile.component';
 import { ScheduleComponent } from './schedule.component';
 
 describe('ScheduleComponent', () => {
@@ -8,9 +13,13 @@ describe('ScheduleComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ScheduleComponent ]
-    })
-    .compileComponents();
+      declarations: [ScheduleComponent, DayComponent, TileComponent],
+      imports: [getTranslocoModule(), MatGridListModule],
+      providers: [
+        { provide: MATERIAL_SANITY_CHECKS, useValue: false },
+        { provide: ToolbarService, useValue: { title: '' } }
+      ]
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +30,11 @@ describe('ScheduleComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set toolbar title on init', () => {
+    component.ngOnInit();
+    const toolbarService = TestBed.inject(ToolbarService);
+    expect(toolbarService.title).toBe('schedule.title');
   });
 });
--- apps/xantar-client/src/app/modules/schedule/components/schedule/schedule.component.ts
+++ apps/xantar-client/src/app/modules/schedule/components/schedule/schedule.component.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 import { ISchedule } from '@xantar/domain/models';
 import { Observable, of } from 'rxjs';
+import { ToolbarService } from '../../../shared/services/toolbar/toolbar.service';
 import { ScheduleService } from '../../services/schedule/schedule.service';
 
 @Component({
   selector: 'xantar-schedule',
   templateUrl: './schedule.component.html',
-  styleUrls: ['./schedule.component.scss']
+  styleUrls: ['./schedule.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ScheduleComponent implements OnInit {
-  constructor(private scheduleService: ScheduleService) {}
+  constructor(
+    private scheduleService: ScheduleService,
+    private toolbarService: ToolbarService,
+    private translocoService: TranslocoService
+  ) {}
 
   public schedule$!: Observable<ISchedule>;
 
   ngOnInit(): void {
     this.fetchSchedule();
+    this.toolbarService.title =
+      this.translocoService.translate('schedule.title');
   }
 
   public fetchSchedule() {
